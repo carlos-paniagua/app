f2856d660b98766b611b19e82da06a8fd3af84b8
carlos_paniagua <carlos.paniagua042137@gmail.com>
2023-05-16 01:51:57 +0000

    app.py 追加

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///food.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///food.db' #データベースURI設定
 db = SQLAlchemy(app)
+    
+class Post(db.Model): #データベースのテーブル設定
+    id = db.Colum(db.Integer,primary_key=True)
+    food_name = db.Column(db.String(30),nullable=False)
+    cost = db.Column(db.Integer)
+    detail = db.Column(db.String)
+    due = db.Column(db.DateTime,nullable=False)
+    category = db.Column(db.String(30),nullable=False)
+    save_type = db.Column(db.String(30))
+    
 
 @app.route('/')
 def index():
@@ -11,9 +21,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
-class Post(db.Model):
-    id = db.Colum(db.Integer,primary_key=True)
-    food_name = db.Column(db.String(30),nullable=False)
-    cost = 
-    detail = 
