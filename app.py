508cb6e2027d070d8125c90b800a2b9a57ae30d5
carlos_paniagua <carlos.paniagua042137@gmail.com>
2023-05-16 08:23:52 +0000

    app.py　'/', methods=['GET', 'POST'] 追加

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///food.db' #データベースURI設定
@@ -9,19 +10,23 @@ class Post(db.Model): #データベースのテーブル設定
     id = db.Column(db.Integer,primary_key=True)
     food_name = db.Column(db.String(30),nullable=False)
     cost = db.Column(db.Integer)
-    weight = db.Column(db.Intenger)
-    count = db.Column(db.Intenger)
+    weight = db.Column(db.Integer)
+    count = db.Column(db.Integer)
     due = db.Column(db.DateTime,nullable=False)
     category = db.Column(db.String(30),nullable=False)
     save_type = db.Column(db.String(30))
     
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == "GET":
+        
+    else:
+        
     return render_template('index.html')
 
 @app.route('/create')
-def index():
+def create():
     return render_template('create.html')
 
 if __name__ == "__main__":
