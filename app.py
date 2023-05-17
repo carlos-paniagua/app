0dbffb63117b67897dc5dae0091ed054ce1d2bbe
carlos-paniagua <carlos_paniagua0421@hotmail.com>
2023-05-17 10:53:35 +0000

    画像をデータベースに保存する機能を追加

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
+from werkzeug.utils import secure_filename
 from datetime import datetime, date
+import uuid
+
+UPLOAD_FOLDER = '/static/image/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+DEBUG_MODE = True
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///food.db' #データベースURI設定
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_ECHO']=True
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'secret_key'
 db = SQLAlchemy(app)
-bootstrap = Bootstrap(app)
 
 class Post(db.Model): #データベースのテーブル設定
     __tablename__ = 'food_info'
@@ -21,7 +28,10 @@ class Post(db.Model): #データベースのテーブル設定
     due = db.Column(db.DateTime,nullable=False)
     category = db.Column(db.String(30))
     save_type = db.Column(db.String(30))
-    # image_path = db.Column(db.String(200))
+    image_path = db.Column(db.String(200))
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -34,27 +44,44 @@ def index():
     
     #保存されているタスクを表示する
     else:
-        food_name = request.form.get("food_name")
-        cost = request.form.get("cost")
-        weight = request.form.get("weight")
-        count =  request.form.get("count")
-        due = request.form.get("due")
-        category = request.form.get("category")
-        save_type = request.form.get("save_type")
-        # image = request.files('image')
+        if 'image' not in request.files:
+            print('No image part')
+            return redirect("/")
         
-        # 画像を保存するパスを作成
-        image_path = 'static/images/' + image.filename
-
-        # 画像を指定のパスに保存
-        image.save(image_path)
+        image = request.files['image']
+        if image.filename == '':
+            print('No selected image')
+            return redirect("/")
         
-        due = datetime.strptime(due,"%Y-%m-%d")
-        new_post = Post(food_name=food_name,cost=cost,weight=weight,count=count,due=due,category=category,save_type=save_type)#image_path=image_path
-        
-        db.session.add(new_post) #内容を追加
-        db.session.commit() #反映
-        return redirect("/") #トップページへリダイレクト
+        if image and allowed_file(image.filename):
+            food_name = request.form.get("food_name")
+            cost = request.form.get("cost")
+            weight = request.form.get("weight")
+            count =  request.form.get("count")
+            due = request.form.get("due")
+            category = request.form.get("category")
+            save_type = request.form.get("save_type")
+            
+            due = datetime.strptime(due,"%Y-%m-%d")
+            extension = image.filename.rsplit('.', 1)[1].lower()
+            random_filename = str(uuid.uuid4()) + '.' + extension
+            image_path = UPLOAD_FOLDER + random_filename
+            image.save(image_path)
+            
+            new_food = Post(
+                food_name=food_name,
+                cost=cost,
+                weight=weight,
+                count=count,
+                due=due,
+                category=category,
+                save_type=save_type,
+                image_path=image_path
+            )
+            
+            db.session.add(new_food) #内容を追加
+            db.session.commit() #反映
+            return redirect("/") #トップページへリダイレクト
 
 @app.route('/create')
 def create():
@@ -62,7 +89,6 @@ def create():
 
 @app.route('/detail/<int:id>')
 def read(id):
-   
     post = Post.query.get(id) # 該当するidの投稿内容を取得
     return render_template("detail.html",post=post) # 、タスクをdetail.htmlに渡す
 
@@ -91,7 +117,8 @@ def update(id):
         return redirect("/")
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    # db.create_all()
+    app.run(debug=DEBUG_MODE)
     app.run(host='0.0.0.0', port=5000) 
     
 # from app import app,db
