ffe58359cc0de477d46e1723663f85e322449889
carlos-paniagua <carlos_paniagua0421@hotmail.com>
2023-05-17 03:13:52 +0000

    delete update機能追加

--- app.py
+++ app.py
@@ -21,10 +21,14 @@ class Post(db.Model): #データベースのテーブル設定
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    
+    #データベースにタスクを保存
     if request.method == "GET":
-        posts = Post.query.all()    #データベースからすべての情報を取りだし，トップページに渡す
+        #データベースからすべての情報を取りだし，トップページに渡す
+        posts = Post.query.order_by(Post.due).all() #期限が近い順
         return render_template("index.html",posts=posts)
     
+    #保存されているタスクを表示する
     else:
         food_name = request.form.get("food_name")
         cost = request.form.get("cost")
@@ -39,11 +43,41 @@ def index():
         
         db.session.add(new_post) #内容を追加
         db.session.commit() #反映
-        return redirect("/")
+        return redirect("/") #トップページへリダイレクト
 
 @app.route('/create')
 def create():
     return render_template('create.html')
 
+@app.route('/detail/<int:id>')
+def read(id):
+   
+    post = Post.query.get(id) # 該当するidの投稿内容を取得
+    return render_template("detail.html",post=post) # 、タスクをdetail.htmlに渡す
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    post = Post.query.get(id) # 該当するidの投稿内容を取得
+    db.session.delete(post)   #内容削除
+    db.session.commit()     #反映
+    return redirect("/")    #トップページへリダイレクト
+    
+@app.route("/update/<int:id>",methods=["GET","POST"])
+def update(id):
+    post = Post.query.get(id)
+    
+    #今まで書かれていた内容を表示
+    if request.method == "GET":
+        return render_template("update.html",post=post)
+    
+    #変更内容を更新する
+    else:
+        post.food_name=request.form.get("food_name")
+        post.cost=request.form.get("cost")
+        post.due=datetime.strptime(request.form.get("due"),"%Y-%m-%d")
+        
+        db.session.commit()
+        return redirect("/")
+    
 if __name__ == "__main__":
     app.run(debug=True)
